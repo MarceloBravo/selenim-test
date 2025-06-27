5c3e0018c505b0a0f30f60b076f0432cb9d9bed1
Marcelo Bravo <mabc@live.cl>
2025-06-26 23:08:52 -0400

    Primera corrección al error del github actons

--- test_busqueda.py
+++ test_busqueda.py
@@ -1,11 +1,19 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")  # Ejecutar sin interfaz gráfica
+chrome_options.add_argument("--user-data-dir=/tmp/chrome-profile")
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://duckduckgo.com/")
 
 # Buscar campo de texto
