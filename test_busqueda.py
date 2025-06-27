8677700ea6942b83db02ed53e94ee7353a4df3a3
Marcelo Bravo <mabc@live.cl>
2025-06-26 23:18:12 -0400

    Se cambio la palabra de búsqueda en el archivo test_busqueda.oy

--- test_busqueda.py
+++ test_busqueda.py
@@ -23,11 +23,11 @@ buscador.send_keys(Keys.RETURN)
 
 # Esperar hasta que aparezcan los resultados
 WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, ".react-results--main"))
+    EC.presence_of_element_located((By.CSS_SELECTOR, ".results"))
 )
 
 # Validar que exista algún resultado
-resultados = driver.find_elements(By.CSS_SELECTOR, ".react-results--main")
+resultados = driver.find_elements(By.CSS_SELECTOR, ".results")
 print(driver.page_source)  # Imprime el HTML actual de la página
 assert len(resultados) > 0, "No se encontraron resultados."
 
